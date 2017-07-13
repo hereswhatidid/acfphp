7b6debbaaf516604c06f9f19a87e32addab373e3
Gabe Shackle <gabe@risdall.com>
2017-07-13 08:26:08 -0500

    Added new Link type field (ACF 5.6+)

---
+++ v5/field_types/link.php
@@ -0,0 +1,16 @@
+<?php
+array (
+	'key' => 'FIELDKEY',
+	'label' => 'Link',
+	'name' => 'link',
+	'type' => 'link',
+	'instructions' => '',
+	'required' => 0,
+	'conditional_logic' => 0,
+	'wrapper' => array (
+		'width' => '',
+		'class' => '',
+		'id' => '',
+	),
+	'return_format' => 'array',
+),
