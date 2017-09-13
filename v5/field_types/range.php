07c0de17c11ac928cdb4b0d5e523fd947848f192
Gabe Shackle <gabe@risdall.com>
2017-09-13 07:58:46 -0500

    Added new Range field type.

---
+++ v5/field_types/range.php
@@ -0,0 +1,21 @@
+<?php
+array (
+	'key' => 'FIELDKEY',
+	'label' => __( 'Range', 'textdomain' ),
+	'name' => 'range',
+	'type' => 'range',
+	'instructions' => '',
+	'required' => 0,
+	'conditional_logic' => 0,
+	'wrapper' => array (
+		'width' => '',
+		'class' => '',
+		'id' => '',
+	),
+	'default_value' => '',
+	'min' => '', // number, default: 0
+	'max' => '', // number, default: 100
+	'step' => '', // number, default: 1
+	'prepend' => '',
+	'append' => '',
+),
